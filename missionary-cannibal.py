444fe32cb7bbc155f3836a0052c13b76d4962b73
Corey Sokol <corey_sokol@icloud.com>
2018-08-19 22:35:58 -0500

    Creates a graph and connects all possible turn scenarios

--- missionary-cannibal.py
+++ missionary-cannibal.py
@@ -20,11 +20,11 @@ class Vertex:
     
     
 class Graph:
-    def __init__(self):
+    def __init__(self):               # initializes the graph itself and creates an empty list of vertices
         self.vertList = {}
         self.numVertices = 0
 
-    def addVertex(self,key):
+    def addVertex(self,key):          # creates a new vertex and calls the Vertex class
         self.numVertices = self.numVertices + 1
         newVertex = Vertex(key)
         self.vertList[key] = newVertex
@@ -39,7 +39,7 @@ class Graph:
     def __contains__(self,n):
         return n in self.vertList
 
-    def addEdge(self,f,t,cost=0):
+    def addEdge(self,f,t,cost=0):           # checks if edge point is not in the existing verticy list
         if f not in self.vertList:
             nv = self.addVertex(f)
         if t not in self.vertList:
@@ -56,24 +56,28 @@ class Graph:
         return str(self.vertList)
 
 
-    
+# declare the graph
 g = Graph()
     
-
+# possible moves for this game are added into a list
 possibleMoves = ["331", "330", "231", "230", "321", "320", "221", "220", "231", "121", "120", "111", "110", "011", "010", "101", "100", "000"]
 
+# creates vertices for each move in the game
 for i in possibleMoves:
     g.addVertex(i)
-    
+
+# we connect each possible move in the game with eachother using the for loop below
 for i in possibleMoves:
     for n in possibleMoves:
         if n != i:
             g.addEdge(str(i), str(n))
 
-
+# for testing purposes we're checking the vertices that exist
 for v in g:
     print(v)
 
+
+# the code below is for testing purposes
 combinations = g.getVertices()
 
 boatState = 0
